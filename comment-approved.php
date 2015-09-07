271b379dd4d14c988fe7d5d9c66955607fe75254
Niels van Renselaar <nielsvanrenselaar@media-enzo.nl>
2015-09-07 12:37:01 +0200

    Fixed translation issue

--- comment-approved.php
+++ comment-approved.php
@@ -4,7 +4,7 @@ Plugin Name: Comment Approved
 Plugin URI: https://nielsvr.com
 Description: Notify a user when their comment is approved
 
-Version: 1.4
+Version: 1.4.1
 Requires at least: 3.0
 
 Author: Niels van Renselaar
